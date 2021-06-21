1f16b612fc47d3e863f79653aeace7dbc62b4bde
Spencer Nowak <snowak@vmware.com>
2021-06-21 10:37:47 -0400

    Update to v3.0.4 and document local differences (#1)

--- karma.conf.js
+++ karma.conf.js
@@ -15,12 +15,12 @@ module.exports = function(config) {
         logLevel: config.LOG_INFO,
         autoWatch: true,
         customLaunchers: {
-            ChromeTravis: {
+            Chrome_Travis: {
                 base: 'Chrome',
                 flags: ['--no-sandbox']
             }
         },
-        browsers: ['PhantomJS2'],
+        browsers: ['PhantomJS'],
         singleRun: false
     });
 };
